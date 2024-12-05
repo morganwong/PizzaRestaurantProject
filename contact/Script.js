3b5909d35ff28e5200157030c9cbd95ec7205e8e
09higginsg <x24159808@student.ncirl.ie>
2024-12-05 00:01:28 +0000

    Update Script.js
    Updated for new reservation page

--- contact/Script.js
+++ contact/Script.js
@@ -1,14 +1,17 @@
 // When Submit is pressed by customer
-$("#Contact-Form").submit(function(event) {
-  event.preventDefault(); // Prevent the default form submission
+$("#Contact-Form").submit(function (event) {
+  event.preventDefault(); 
 
-  // Get customer's name
+  // Get customer name
   var name = $("#name").val();
 
-  // Customer thank you message
+  // Display confirmation message
   var confirmationMessage = $("#confirmationMessage");
-  confirmationMessage.text("Hi " + name + ". Thank you for contacting us! Your message has been received. Our Perfecto Pizza team will contact you soon!");
-  confirmationMessage.removeClass("d-none"); 
+  confirmationMessage.text(
+      "Hi " + name + 
+      ". Thank you for booking with us! Our Perfecto Pizza team will contact you soon to confirm your reservation!"
+  );
+  confirmationMessage.removeClass("d-none");
 
   // Reset the form
   $("#Contact-Form")[0].reset();
