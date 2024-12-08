30bced1f6e455859e70ca7c060df6259eb47caf8
09higginsg <x24159808@student.ncirl.ie>
2024-12-08 13:20:59 +0000

    Update Script.js

--- contact/Script.js
+++ contact/Script.js
@@ -1,10 +1,17 @@
-// When Submit is pressed by customer
+// After submit is pressed
 $("#Contact-Form").submit(function (event) {
   event.preventDefault(); 
 
-  // Get customer name
+  // Get customer name/date and todays date
   var name = $("#name").val();
+  var date = $("#date").val();
+  var today = new Date().toISOString().split('T')[0]; 
 
+  // Check customers pref date against todays date
+  if (date < today) {
+    alert("Looks like you have tried to book a date in the past! Please select a valid date and time!" );
+    return;
+  }
   // Display confirmation message
   var confirmationMessage = $("#confirmationMessage");
   confirmationMessage.text(
